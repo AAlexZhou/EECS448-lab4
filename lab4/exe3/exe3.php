fe38d4cb49858278de330c2b0b0d78d1c3ca0f32
jialei zhou <47931522+AAlexZhou@users.noreply.github.com>
2019-10-07 13:21:06 -0500

    Update exe3.php

--- lab4/exe3/exe3.php
+++ lab4/exe3/exe3.php
@@ -1,48 +1,59 @@
 <?php
-echo "<link rel="stylesheet" href="exe3.css" type="text/css"/>";
-$a=$_POST["Anum"];
-$b=$_POST["Bnum"];
-$l=$_POST["Lnum"];
-$shopping=$_POST["shopping"];
+echo '<link rel="stylesheet" href="exe3.css" type="text/css"></head>';
+
+  $a = $_POST["Anum"];
+  $b = $_POST["Bnum"];
+  $l = $_POST["Lnum"];
+  $shopping = $_POST["shopping"];
+  $password = $_POST["password"];
+
 $money= 0;
-$money=(a*1)+(b*2)+(l*3);
-if ($shopping==50)
+$bp = $b * 2;
+$lp = $l * 3;
+$money=$a +$bp +$lp;
+$shopc=0;
+if ($shopping=="50")
 {
   $money=$money+50;
+  $shopc=50;
 }
-if ($shopping==5)
+else if ($shopping=="5")
 {
   $money=$money+5;
+  $shopc=5;
 }
 echo "Thank you for shopping<br>";
 echo "your password is:".$password."<br>";
 echo "<table>";
 echo "<tr>";
-echo "<th> </th>";
+echo "<th>Item</th>";
 echo "<th>Quantity</th>";
 echo "<th>Cost Per Item</th>";
 echo "<th>Sub Total</th>";
 echo "</tr>";
 echo "<tr>";
 echo "<td>apple</td>";
+echo "<td>".$a."</td>";
 echo "<td>1</td>";
 echo "<td>".$a."</td>";
 echo "</tr>";
 echo "<tr>";
 echo "<td>banana</td>";
+echo "<td>".$b."</td>";
 echo "<td>2</td>";
-echo "<td>".$b*2."</td>";
+echo "<td>".$bp."</td>";
 echo "</tr>";
 echo "<tr>";
 echo "<td>lemon</td>";
+echo "<td>".$l."</td>";
 echo "<td>3</td>";
-echo "<td>".$l*3."</td>";
+echo "<td>".$lp."</td>";
 echo "</tr>";
 echo "<tr>";
 echo "<td>shipping</td>";
 echo "<td> </td>";
 echo "<td> </td>";
-echo "<td>".$shopping."</td>";
+echo "<td>".$shopc."</td>";
 echo "</tr>";
 echo "<td>Total cost</td>";
 echo "<td> </td>";
@@ -51,3 +62,4 @@ echo "<td>".$money."</td>";
 echo "</tr>";
 echo "</table>";
  ?>
+
